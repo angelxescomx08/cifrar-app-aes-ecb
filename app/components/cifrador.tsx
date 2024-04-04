aa7c24ad5a5d85bc21922c2cfee11085a7f00d97
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-04-04 14:15:27 -0600

    estructura html

---
+++ app/components/cifrador.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Cifrador = () => {
+  return (
+    <form>
+      <input placeholder="Llave" type="text" />
+      <input placeholder="Texto plano" type="text" />
+      <button>Cifrar</button>
+    </form>
+  );
+};
---
+++ app/components/descifrador.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Descifrador = () => {
+  return (
+    <form>
+      <input placeholder="Llave" type="text" />
+      <input placeholder="Texto cifrado" type="text" />
+      <button>Descifrar</button>
+    </form>
+  );
+};
