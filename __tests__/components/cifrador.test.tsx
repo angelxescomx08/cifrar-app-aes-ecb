daff2fc5b71a4965b0e2d8e8e823dc89203496ff
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-01-06 20:36:26 -0600

    update: render cipher

---
+++ __tests__/components/cifrador.test.tsx
@@ -0,0 +1,17 @@
+import { Cifrador } from "@/app/components/cifrador";
+import { MantineProvider } from "@mantine/core";
+import { render } from "@testing-library/react";
+
+function Wrapper() {
+	return (
+		<MantineProvider>
+			<Cifrador />
+		</MantineProvider>
+	);
+}
+
+describe("Cifrador", () => {
+	it("Shoud render", () => {
+		render(<Wrapper />);
+	});
+});
